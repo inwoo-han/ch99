d9d9fa1b3fba9a84eca589eabda021334588ab7d
inwoo-han <eve9509@nate.com>
2021-03-26 17:43:47 +0900

    redbook_0326

--- bookmark/models.py
+++ bookmark/models.py
@@ -4,9 +4,11 @@ import datetime
 # Create your models here.
 
 class Bookmark(models.Model):
+    #id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID_NUM')
     title = models.CharField("TITLE", max_length=100, blank=True)
     url = models.URLField('URL', unique=True)
     date = models.DateTimeField('DATETIME', default=datetime.datetime.now, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        #return self.title
+        return "%s %s" %(self.title, self.url)
