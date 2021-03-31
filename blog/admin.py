25762a4fada142c7a0a4fb06230478799678aca9
inwoo-han <eve9509@nate.com>
2021-03-31 16:01:47 +0900

    Blog_practice

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'modify_dt')
+    list_filter = ('modify_dt',)
+    search_fields = ('title','content')
+    prepopulated_fields = {'slug': ('title',)}  # 포스트 추가할때 타이틀에 맞춰 자동완성 되는 필드
